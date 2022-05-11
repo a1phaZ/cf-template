bb9981c88666a993df2db5d7b30006e330020694
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2022-05-11 17:17:01 +0300

    getKeys & getValues

--- src/queries/clients-table.ts
+++ src/queries/clients-table.ts
@@ -1,5 +1,5 @@
 import { driver } from '../database';
-import { TypedData, Types } from 'ydb-sdk';
+import { Types } from 'ydb-sdk';
 import { TableDescription2 } from './helpers';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -21,15 +21,26 @@ export async function createClientTable(apiKey: string) {
   });
 }
 
-export async function insertValues(apiKey: string) {
+export async function insertValues(apiKey: string, data: unknown) {
   // для первичного ключа отлично подходит uuid
   // UPSERT и REPLACE являются операциями модификации данных, которые не требует их предварительного чтения, за счет чего работают быстрее и дешевле других операций.
   const query = `
-    upsert INTO ${apiKey} (products_id, products_name, price_baz)
-    VALUES ('${uuidv4()}', 'Яблочный край', 1414);
+    upsert INTO ${apiKey} (id, '${getKeys(data)}')
+    VALUES ('${uuidv4()}', '${getValues(data)}');
   `;
 
   await driver.tableClient.withSession(async (session) => {
     await session.executeQuery(query);
   });
+}
+
+function getKeys(data: unknown): string {
+  return Object.keys(data).join(', ');
+}
+
+function getValues(data: unknown): string {
+  const result = Object.keys(data).reduce((acc, cur) => {
+    return [...acc, data[cur]];
+  }, []);
+  return result.join(', ');
 }
